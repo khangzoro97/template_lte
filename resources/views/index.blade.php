38b950e85912dcac37a428befc8bdcf16e26f75e
khang <khangzoro97@gmail.com>
2021-11-26 14:41:30 +0700

    update_25112021

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Trang chủ')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
+@section('content')
+
+    <!-- Main row -->
+
+
+    <!-- /.row (main row) -->
+@stop
+
+@section('css')
+@stop
+
+@section('js')
+@stop
